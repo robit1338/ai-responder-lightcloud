2770c4c475fb50988ebe9a207a593e4947fff616
IToP4 <73798424+robit1338@users.noreply.github.com>
2026-02-09 18:09:52 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,36 +1,16 @@
 @echo off
 chcp 65001 > nul
-
-REM Перейти в директорию файла (корень проекта)
 pushd "%~dp0"
 
-REM Создать venv только если его нет
 if not exist venv (
-    echo [INFO] Creating venv...
     python -m venv venv
-    if errorlevel 1 (
-        echo [ERROR] Failed to create venv. Make sure python is in PATH.
-        popd
-        exit /b 1
-    )
 )
 
-REM Активировать venv
 call venv\Scripts\activate
 
-REM Установить только зависимости из requirements.txt (без лишних апгрейдов)
-echo [INFO] Installing requirements...
 python -m pip install -r requirements.txt
-if errorlevel 1 (
-    echo [ERROR] Installing requirements failed.
-    popd
-    exit /b 2
-)
 
-REM Запуск бота
-echo [INFO] Starting bot...
-python -m start.windows.start_windows
+REM Запуск Python в отдельном процессе cmd
+cmd /c python -m start.windows.start_windows
 
-REM Возврат в исходную папку
 popd
-exit /b 0
